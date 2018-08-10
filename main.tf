a45d9a166968837fe29a36451e377dc73980848b
manibrar <manvirbrar1988@gmail.com>
2018-08-10 11:09:12 +0100

    user data db to app

--- main.tf
+++ main.tf
@@ -2,11 +2,26 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-resource "aws_instance" "app" {
+data "template_file" "app_user_data" {
+  template = "${file("${path.module}/templates/app/user_data.sh.tpl")}"
+  vars {
+    db_host = "mongodb://${aws_instance.db_instance.private_ip}:27017/posts"
+  }
+}
+
+resource "aws_instance" "app-instance" {
   ami = "${var.app_ami_id}"
   instance_type = "t2.micro"
-
+  user_data = "${data.template_file.app_user_data.rendered}"
   tags {
     Name = "Manvir-app"
+  }
+}
+
+resource "aws_instance" "db_instance" {
+  ami = "${var.db_ami_id}"
+  instance_type = "t2.micro"
+  tags {
+    Name = "Manvir-db"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "app_ami_id" {
   description = "ami id for the app instance"
   default = "ami-c2b8bfbb"
 }
+
+variable "db_ami_id" {
+  description = "ami id for the db instance"
+  default = "ami-01020378"
+}
